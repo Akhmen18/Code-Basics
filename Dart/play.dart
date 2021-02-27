d0c08fbc96d87a8294dfd10b01084394dab89387
Akhil Menon <akh180599@gmail.com>
2021-02-27 12:37:37 +0530

    Add Functions for reference

--- Dart/play.dart
+++ Dart/play.dart
@@ -3,4 +3,16 @@ main(){
 
   int a = 12;
   print(a);
+
+  secondLevelFunctions(10);
+
 }
+
+  // Functions
+  void basicFunction(){
+
+  }
+
+  void secondLevelFunctions(int n){
+    print('Number is: $n');
+  }
