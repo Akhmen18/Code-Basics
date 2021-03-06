96db289e59bd56e6aad82371cb10a78e5330dd7e
Akhil Menon <akh180599@gmail.com>
2021-03-06 15:03:44 +0530

    Refactor

--- Dart/play.dart
+++ Dart/play.dart
@@ -27,6 +27,19 @@ main(){
   myStringList.insert(2, '0605LOLOLOL');
   print(myStringList);
 
+  final int avar = 1;
+  const int bvar = 3; 
+
+  /* Constants cannot be changed after its created. 
+      Same goes with Final. A final variable can be set only once.
+      Whereas a const is a compile-time constant
+      A const can't have aceess to anything during runtime
+      eg: const b = DateTime.now() won't work properly.
+      Whereas final b = DateTime.now() will work as at any point of time during the execution
+      the DateTime can be called for the object. Whereas with const, its calculated during compilation which wont give us the DateTime required.*/
+
+  print(avar);
+  print(bvar);
 }
 
   // Functions
@@ -43,3 +56,4 @@ main(){
     int add = a + b;
     print('Addition is: $add');
   }
+
